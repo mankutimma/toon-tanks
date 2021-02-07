404359dc09bf9870b2b65740c8199ecd776a484b
Ashish Jagadish (RBEI/ESU5) <Ashish.Jagadish@in.bosch.com>
2021-02-07 19:15:28 +0530

    first commit

---
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -0,0 +1,29 @@
+// Copyright 2021 Ashish Jagadish, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PawnBase.generated.h"
+
+UCLASS()
+class TOONTANKS_API APawnBase : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APawnBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
