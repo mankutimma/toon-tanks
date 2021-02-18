5bc65ae2cbc800a253af8eba1dfb9fd78747cbd8
Ashish Jagadish <Ashish.Jagadish@protonmail.com>
2021-02-18 19:39:51 +0530

    created basic components

--- Source/ToonTanks/Pawns/PawnBase.cpp
+++ Source/ToonTanks/Pawns/PawnBase.cpp
@@ -2,12 +2,26 @@
 
 
 #include "PawnBase.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 APawnBase::APawnBase()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	// Since we declared CapsuleComp first, Unreal will flag it as RootComponent and put it at the top of the hierarchy. Nevertheless, for developer reference, assigning it to RootComponent.
+	RootComponent = CapsuleComp;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	BaseMesh->SetupAttachment(RootComponent); // We want BaseMesh ti inherit the movement of the capsule component below it.
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+	TurretMesh->SetupAttachment(BaseMesh);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn Point"));
+	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
 
 }
 
--- Source/ToonTanks/Pawns/PawnBase.h
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -6,24 +6,39 @@
 #include "GameFramework/Pawn.h"
 #include "PawnBase.generated.h"
 
+// This is a forward declaration. A forward declaration is used instead of including a header file when variables/methods of the class do not have to be accessed.
+// So, it is just used as a reference so that the compiler doesn't freak out when we use this class in our own APawnBase class.
+// An advantage of forward declarations over header file includes is that it reduces compile time.
+class UCapsuleComponent;
+
 UCLASS()
 class TOONTANKS_API APawnBase : public APawn
 {
 	GENERATED_BODY()
 
+private:
+	// Capsule component is used for simple collission
+	UPROPERTY()
+	UCapsuleComponent* CapsuleComp = nullptr;
+
+	// We don't need to forward declare static mesh components or scene components since the APawn/Aactor class is already aware of what it is.
+	UPROPERTY()
+	UStaticMeshComponent* BaseMesh = nullptr;
+	UPROPERTY()
+	UStaticMeshComponent* TurretMesh = nullptr;
+	UPROPERTY()
+	USceneComponent* ProjectileSpawnPoint = nullptr; //Scene component is a collection of data essentially with transforms so that we can place the projectile at a certain spawn point
+
 public:
 	// Sets default values for this pawn's properties
 	APawnBase();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 };
